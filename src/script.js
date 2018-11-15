ceba2dba458437430a2b254b8d14deaccfd0be5d
exurd <alastercaslin@gmail.com>
2018-11-15 23:29:34 +0000

    Add websites - Batch 1
    Welcome to the stage where the websites are added.
    This is Batch 1.
    
    Side note, if anyone knows how to make a json file for the links and have the javascript read it and randomly choose one then redirect to that site, then please make a pull request. That would mean a lot to me!

--- src/script.js
+++ src/script.js
@@ -1,8 +1,37 @@
 var sites, index;
 sites = [
-        "http://google.com",
-        "http://yahoo.com",
-        "http://bing.com" ];
+        "https://www.pointerpointer.com",
+        "https://heeeeeeeey.com",
+        "http://hackertyper.com",
+        "http://www.staggeringbeauty.com",
+        "http://beesbeesbees.com",
+        "http://www.drivemeinsane.com",
+        "https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles",
+        "https://en.wikipedia.org/wiki/Wikipedia:List_of_really,_really,_really_stupid_article_ideas_that_you_really,_really,_really_should_not_create",
+        "http://www.unforgettable.dk",
+        "https://thisissand.com",
+        "http://orteil.dashnet.org/cookieclicker/",
+        "https://www.incredibox.com",
+        "http://freerice.com",
+        "https://www.rainymood.com",
+        "http://good-v-evil.com",
+        "http://ww3.safestyle-windows.co.uk/the-secret-door/",
+        "http://savethesounds.info",
+        "https://ineedaprompt.com",
+        "https://mrdoob.com/projects/chromeexperiments/google-gravity/",
+        "https://mrdoob.com/projects/chromeexperiments/google-space/",
+        "https://mrdoob.com/lab/javascript/effects/ie6/",
+        "https://mrdoob.com/lab/javascript/effects/solitaire/",
+        "https://mrdoob.com/lab/javascript/webgl/voxels_liquid/",
+        "http://www.windows93.net",
+        "http://thenostalgiamachine.com",
+        "https://trypap.com",
+        "http://burymewithmymoney.com",
+        "https://cant-not-tweet-this.com",
+        "https://thatsthefinger.com",
+        "http://eelslap.com",
+        "http://www.republiquedesmangues.fr",
+        "https://findtheinvisiblecow.com" ];
 
 index = Math.floor(Math.random() * sites.length);
 
